dc5747b505e8e35000bee9abaf0dd5b118172849
Gustas <gustass1@gmail.com>
2022-03-23 22:49:22 +0100

    M-4: Created Dashboard page

--- resources/js/Pages/Login.tsx
+++ resources/js/Pages/Login.tsx
@@ -76,7 +76,12 @@ const Login: React.FC = () => {
 									type="submit"
 									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-700 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 								>
-									Sign in
+									<a
+										href="/dashboard"
+										className="flex flex-grow justify-center items-center"
+									>
+										Sign in
+									</a>
 								</button>
 							</div>
 						</form>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return Inertia::render('Login');
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+});
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
