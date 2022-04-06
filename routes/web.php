7bccee06bc28323cdaf7c55a205efaa23f927d6b
Gustas <gustass1@gmail.com>
2022-04-06 23:50:05 +0200

    M-47: Updated routes with authentication, login and register pages

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return Inertia::render('Login');
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+
+Route::get("/", function () {
+	return redirect("login");
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::middleware(["guest"])->group(function () {
+	Route::get("/login", function () {
+		return Inertia::render("Login");
+	})
+		->name("login")
+		->middleware("guest");
+
+	Route::post("/login", [LoginController::class, "authenticate"]);
+
+	Route::get("/register", function () {
+		return Inertia::render("Register");
+	});
+	Route::post("/register", [RegisterController::class, "register"]);
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::middleware(["auth"])->group(function () {
+	Route::get("/dashboard", function () {
+		return Inertia::render("Dashboard");
+	})
+		->name("dashboard")
+		->middleware("auth");
+
+	Route::post("/logout", [LogoutController::class, "logout"]);
+});
+
+Route::get("/welcome", function () {
+	return view("welcome");
 });
