2382b6a1518d6e4e0084b660966ac326b498c4e0
luksys3 <theluksys3@gmail.com>
2022-04-26 01:22:11 +0200

    M-84: Added charts

--- resources/js/layout/AuthenticatedLayout.tsx
+++ resources/js/layout/AuthenticatedLayout.tsx
@@ -99,7 +99,7 @@ export const AuthenticatedLayout: React.FC<AuthenticatedLayoutInterface> = ({
 						</div>
 					</header>
 
-					<div className="w-full py-6 sm:px-6 lg:px-8 overflow-y-scroll">
+					<div className="w-full py-6 sm:px-6 lg:px-8">
 						{children}
 					</div>
 				</main>
