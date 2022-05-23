d600c72516e8bb9e174f7bbe6fb08e28f4488003
luksys3 <theluksys3@gmail.com>
2022-05-23 12:06:50 +0200

    M-136: add dummy endpoint for receiving room's results

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Domain\API\Controllers\GetRoomWithAccessCodeController;
 use Domain\API\Controllers\GetTestPostsController;
+use Domain\API\Controllers\ReceiveRoomResultsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/test/posts", GetTestPostsController::class);
+Route::post(
+	"/rooms/{room:access_code}/results",
+	ReceiveRoomResultsController::class
+);
 
 Route::get("/rooms/{room:access_code}", GetRoomWithAccessCodeController::class);
