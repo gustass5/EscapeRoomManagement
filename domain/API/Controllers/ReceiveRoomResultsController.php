9cb9b70990df7c3555f82bf811cc23ed8a2ba7ea
Gustas <gustass1@gmail.com>
2022-05-31 09:50:35 +0200

    M-11: Merged development

--- domain/API/Controllers/ReceiveRoomResultsController.php
+++ domain/API/Controllers/ReceiveRoomResultsController.php
@@ -2,13 +2,36 @@
 
 namespace Domain\API\Controllers;
 
+use App\Models\Room;
 use Domain\API\Classes\ApiJsonResponse;
+use Domain\API\Requests\ReceiveRoomResultsRequest;
 use Illuminate\Http\JsonResponse;
 
 class ReceiveRoomResultsController
 {
-	public function __invoke(): JsonResponse
-	{
+	public function __invoke(
+		ReceiveRoomResultsRequest $request,
+		Room $room
+	): JsonResponse {
+		/** @var array{
+		 *     name: string,
+		 *     description: string,
+		 *     incorrectAnswer: int
+		 * } $validated
+		 */
+		$validated = $request->validated();
+
+		$totalAnswersAmount = $room->questions()->count();
+		$correctAnswersAmount =
+			$totalAnswersAmount - $validated["incorrectAnswer"];
+
+		$room->results()->create([
+			"name" => $validated["name"],
+			"description" => $validated["description"],
+			"correct_answers_amount" => $correctAnswersAmount,
+			"total_answers_amount" => $totalAnswersAmount,
+		]);
+
 		return ApiJsonResponse::make()->export();
 	}
 }
